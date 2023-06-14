9a68240a511c91a6ed6267057d443b39a3fe57c2
emre çakmak <45544939+EmreCkm98@users.noreply.github.com>
2023-06-14 16:28:09 +0300

    added news
    added news

---
+++ Object Repository/Imza/Page_Ultia/div_Evlilik zni.rs
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Evlilik zni</name>
+   <tag></tag>
+   <elementGuidId>72a8f098-0a09-454e-8aae-9f0b4d26cf78</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='AbsenceTypeValue_02522d2a-b275-4521-a0d0-1aaadf3f6fe5']/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#AbsenceTypeValue_02522d2a-b275-4521-a0d0-1aaadf3f6fe5 > div.ant-select-item-option-content</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>5246ef54-480e-4e79-b475-e261274fc4c4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>ant-select-item-option-content</value>
+      <webElementGuid>7675e83e-b87d-48a9-a995-45fd467264a2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Evlilik İzni</value>
+      <webElementGuid>77bed52b-daf6-4ef5-a438-86d430ed199a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;AbsenceTypeValue_02522d2a-b275-4521-a0d0-1aaadf3f6fe5&quot;)/div[@class=&quot;ant-select-item-option-content&quot;]</value>
+      <webElementGuid>9d40483a-8a61-4ec4-b0da-fd822b26dc96</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='AbsenceTypeValue_02522d2a-b275-4521-a0d0-1aaadf3f6fe5']/div</value>
+      <webElementGuid>64407e39-c2c5-4a94-8337-b2e737a9377f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Yıllık İzin'])[1]/following::div[2]</value>
+      <webElementGuid>b2f3d7ae-ab44-4692-9b00-33fb8bcff382</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='c394268b-e895-4b28-a978-325396aa2080'])[1]/following::div[10]</value>
+      <webElementGuid>1194b0a4-5a22-4f83-b290-f91c7cac6212</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Hastalık İzni'])[1]/preceding::div[1]</value>
+      <webElementGuid>dc8d4fb9-d12e-4c30-8851-2687c319d11e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Babalık İzni'])[1]/preceding::div[3]</value>
+      <webElementGuid>7f3b9ae5-594d-4aba-81c0-d094924a7d9c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Evlilik İzni']/parent::*</value>
+      <webElementGuid>b9b68ae8-f26b-4e94-a1d5-f490bae66728</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div/div[2]/div</value>
+      <webElementGuid>aa1e5b8d-c35e-4efd-b3e8-f0ec9c4fb90c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Evlilik İzni' or . = 'Evlilik İzni')]</value>
+      <webElementGuid>bd4bcbb9-508e-4202-ba98-1bad06532f91</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
